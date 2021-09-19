91c8549a325d028b060c7473c5d19c9132e28061
Rikzan Fernanda <fernandarikzan@gmail.com>
2021-09-19 11:13:13 +0700

    assets

--- src/views/index.php
+++ src/views/index.php
@@ -4,12 +4,24 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home</title>
+	<link rel="stylesheet" type="text/css" href="<?= BASE_URL . '/assets/css/bootstrap.min.css' ?>">
 </head>
-<body>
-	<?php 
-	echo "<pre>";
-	print_r($data['data']);
-	echo "</pre>";
-	?>
+<body class="bg-light">
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-md-6">
+				<div class="card mt-4">
+					<img src="<?= BASE_URL . '/assets/images/Translator.gif'?>" style="width: 80%; margin: auto">
+					<div class="card-body text-center">
+						<p class="card-text">Selamat Datang</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+	<script type="text/javascript" src="<?= BASE_URL . '/assets/js/jquery-3.6.0.min.js' ?>"></script>
+	<script type="text/javascript" src="<?= BASE_URL . '/assets/js/bootstrap.min.js' ?>"></script>
 </body>
 </html>
